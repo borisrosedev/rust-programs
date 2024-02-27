05063ab1af0077bed7a532c2bf4674674371cb35
boralex <boris.alexandre.rose@gmail.com>
2024-02-27 23:15:32 +0100

    feat/app-one

---
+++ cargo_app_one/src/main.rs
@@ -0,0 +1,24 @@
+// mod guess_game;
+// use guess_game::guess_game;
+
+fn main() {
+
+   let products = ["Fraise","Mangue","Pêche"];
+    let mut index = 0;
+
+
+
+    let iter = products.iter();
+    for product in iter.rev() {
+        println!("{}", product);
+    }
+
+
+
+
+
+    
+  
+
+
+}
